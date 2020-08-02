b9c674d9416e9b9dadad5ffb83b4470c2ee7c85a
Todd Jacobus <rtjacobus1@gmail.com>
2020-08-02 11:05:04 -1000

    added maped service components

--- src/components/services/Service.js
+++ src/components/services/Service.js
@@ -10,22 +10,80 @@ import {
     Link,
 } from 'react-router-dom';
 
-const useStyles = makeStyles(theme => ({
-    root: {},
-}));
+// const useStyles = makeStyles(theme => ({
+//     root: {
+//         width: "100%",
+//     },
+//     imageContainer: {
+//         height: 300,
+//         border: 
+//     },
+//     image: {},
+//     descriptionContainer: {},
+//     description: {},
+// }));
 
 const Service = props => {
+    const { 
+        id,
+        title,
+        tagline,
+        description,
+        image,
+        order,
+     } = props;
+
+    // Control placeholder for image if there's nothing there
+    const [noImageStyle, setNoImageStyle] = React.useState();
+    React.useEffect(() => {
+        image ? setNoImageStyle({}) : setNoImageStyle({
+            // TODO: write some classy css for a image placeholder
+            border: "1px dashed red",
+        })
+    }, [image])
+
+    const useStyles = makeStyles(theme => ({
+        root: {
+            // width: "100%",
+            flexGrow: 1,
+        },
+        imageContainer: {
+            height: 300,
+            textAlign: 'center',
+            ...noImageStyle,
+        },
+        image: {},
+        descriptionContainer: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            height: '100%',
+        },
+        description: {
+            textAlign: 'justify',
+            marginLeft: 50,
+            marginRight: 50,
+
+        },
+    }));
+
     const classes = useStyles();
 
-    const { item } = props;
+    
 
     return (
-        <Grid Container spacing={4}>
-            <Grid Item xs={6}>
-                Image compoment
+        <Grid container spacing={1} className={classes.root}>
+            <Grid item sm={6}>
+                <div className={classes.imageContainer}>
+                    <img src={image} alt={title} className={classes.image} />
+                </div>
             </Grid>
-            <Grid Item xs={6}>
-                Text compoment
+            <Grid item sm={6}>
+                <div className={classes.descriptionContainer}>
+                    <Typography variant="h6" className={classes.description} >
+                        { description }
+                    </Typography>
+                </div>
             </Grid>
         </Grid>
     )
