8f64a38430ea541f697b92aff2a76517e6029c99
Todd Jacobus <rtjacobus1@gmail.com>
2020-08-07 11:07:04 -1000

    fixed margins and other layout for form

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {
-    makeStyles, FormControl, InputLabel, Input, FormHelperText, TextField,
+    makeStyles, FormControl, InputLabel, Input, FormHelperText, TextField, Button,
 
 } from '@material-ui/core';
 
@@ -14,9 +14,22 @@ const useStyles = makeStyles(theme => ({
     },
     formRoot: {
         '& .MuiTextField-root': {
-            margin: theme.spacing(1),
             width: '25ch',
         },
+    },
+    formControl: {
+        '& .MuiFormControl-root': {
+            width: '100%',
+        },
+    },
+    textFieldContainer: {
+        marginTop: 15,
+    },
+    buttonContainer: {
+        textAlign: "center",
+    },
+    button: {
+        marginTop: 100,
     },
 }))
 
@@ -26,26 +39,36 @@ const ContactForm = props => {
 
     return (
         <div className={classes.root}>
-            <FormControl className={classes.formRoot} required >
-                <InputLabel htmlFor="email-input" >Email Address</InputLabel>
-                <Input id="email-input" aria-describedby="email-helper-text" />
-                <FormHelperText id="email-helper-text" >
-                    I'll never share your email
-                </FormHelperText>
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="email-input" >Name</InputLabel>
-                <Input id="name-input" aria-describedby="name-helper-text" />
-            </FormControl>
             <form className={classes.formRoot} >
-                <TextField 
-                    id="comments-field" 
-                    variant="outlined"
-                    label="Comments"
-                    defaultValue="How can I help you meet your business goals?"
-                />
-            </form>
+                <FormControl required className={classes.formControl} >
+                    <InputLabel htmlFor="email-input" >Email Address</InputLabel>
+                    <Input id="email-input" aria-describedby="email-helper-text" />
+                    <FormHelperText id="email-helper-text" >
+                        I'll never share your email
+                    </FormHelperText>
+                </FormControl>
 
+                <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="email-input" >Name</InputLabel>
+                    <Input id="name-input" aria-describedby="name-helper-text" />
+                </FormControl>
+                <div className={classes.textFieldContainer}>
+                    <TextField 
+                        id="comments-field"
+                        label="How can I help?"
+                        multiline
+                    />    
+                </div>
+                <div className={classes.buttonContainer}>
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        className={classes.button}
+                    >
+                        Submit
+                    </Button>
+                </div>
+            </form>
         </div>
     )
 };
